6bd45ba0708dbc804be14f3cad17b84371cfde89
Luv sahu <lksahu404@gmail.com>
2022-07-17 20:53:40 +0530

    [Luv SAHU] This code for SnakeLadder

---
+++ SnakeLadder/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace SnakeandLadder
+{
+    class program
+    {
+        public static void Main (String[]args)
+        {
+
+            Console.WriteLine("==========Welcome To Snake And Ladder Game========");
+            //UC1
+            //SnakeAndLadder snakeAndLadder = new SnakeAndLadder();
+            //snakeAndLadder.StartGame(); // Call the Method.
+            //Console.ReadLine();
+
+
+            //UC2
+            //DiceRolled diceRolled = new DiceRolled();
+            //diceRolled.DieRolled(); // Call the Method.
+            //Console.ReadLine();
+
+            //UC3
+            //CheckOption checkOption = new CheckOption();
+            //checkOption.StartGame(); // Call the Method.
+            //Console.ReadLine();
+
+
+            ////UC4
+            //ReachingPosition100 reachingPosition100 = new ReachingPosition100();
+            //reachingPosition100.StartGame(); // Call the Method.
+            //Console.ReadLine();
+
+
+            ////UC5
+            //ExactWinningPosition exactWinningPosition = new ExactWinningPosition();
+            //exactWinningPosition.StartGame(); // Call the Method.
+            //Console.ReadLine();
+
+
+            ////UC6
+            //Winner winner = new Winner();
+            //winner.StartGame(); // Call the Method.
+            //Console.ReadLine();
+
+
+            //UC7
+            TwoPlayers twoPlayers = new TwoPlayers();
+            twoPlayers.StartGame(); // Call the Method.
+            Console.ReadLine();
+
+
+
+
+
+
+
+        }
+    }
+}
